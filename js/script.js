b14e9dec5873a9b863d4cac6f1074bc636175436
Tets5441 <tetley640@gmail.com>
2018-09-21 11:30:00 +0100

    updated JS to highlight captions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+// video controls
+
 $('video').mediaelementplayer({
     features: ['playpause', 'tracks', 'progress', 'fullscreen', 'volume']
 });
+
+// highlight transcript to match video 
+
+const vid = document.getElementsByTagName('video')[0];
+let caps = document.getElementsByTagName('span');
+
+vid.ontimeupdate = () => {
+  for (let i = 0; i <= caps.length; i ++) {
+    const sync = vid.currentTime;
+    if (sync > caps[i].getAttribute("data-start")
+    && sync < caps[i].getAttribute("data-end")) {
+      caps[i].style.background = "#f4ee42";
+    }
+    else {
+      caps[i].style.background = "";
+    }
+  }
+};
+
+
+
+ 
